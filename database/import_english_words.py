2571cd8c984c39ea4bb555c7ccf9111199843049
li yanru <li.yanru@outlook.com>
2021-01-23 23:28:40 +0800

    english words 8-2

---
+++ database/import_english_words.py
@@ -0,0 +1,81 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+import os
+import sys
+import sqlite3
+import xlrd
+
+script_path = os.path.split(os.path.realpath(sys.argv[0]))[0]
+conn = sqlite3.connect(script_path+"/ggsddu.db")
+ccur = conn.cursor()
+
+
+SUBJECT_VALUE = 3 # 英语为3
+TYPE_VALUE = 1 # 单词题为1
+ENGLISG_WORDS_FILE = script_path+"/../data/english_words.xls"
+
+
+def handle_english_words():
+    workbook = xlrd.open_workbook(ENGLISG_WORDS_FILE)
+    sheet = workbook.sheet_by_index(0)
+    for i in range(1,sheet.nrows):
+        id = sheet.cell(i,0).value
+        des = sheet.cell(i,1).value
+        word = sheet.cell(i,2).value
+        note = sheet.cell(i,3).value
+        
+        is_update = "update" in note
+        is_exist =  query_english_words(id)
+
+        if is_update and is_exist:
+            update_english_words(id, des, word, note)
+        elif not is_exist:
+            new_english_words(id, des, word, note)
+        else:
+            print("%d,%s,%s,%s already exist"%(id, des, word, note))
+
+
+def update_english_words(id, des, word, note):
+    update_new_english_word_sql = \
+        "UPDATE english_words SET DES=?,WORD=?,NOTE=? WHERE ID=%d" % id
+    data_4_english_word_sql = (des,word,note)
+    cur.execute(update_new_english_word_sql,data_4_english_word_sql)
+    conn.commit()
+
+
+def new_english_words(id, des, word, note):
+    insert_new_english_word_sql = \
+        "INSERT INTO english_words(ID,DES,WORD,NOTE)  VALUES(?,?,?,?)"
+    data_4_english_word_sql = (id,des,word,note)
+    cur.execute(insert_new_english_word_sql,data_4_english_word_sql)
+
+    new_id = SUBJECT_VALUE*10**9 + TYPE_VALUE*10**7 + id
+    insert_new_exercise_info_sql = \
+        "INSERT INTO exercise_info(ID,SUBJECT,TYPE,TIMES,CORRECT,STATUS,NOTE) " \
+        "VALUES(?,'english','words',0,0,0,'')"
+    cur.execute(insert_new_exercise_info_sql,(new_id,))
+    
+    conn.commit()
+
+
+def query_english_words(id):
+    """
+    查询是否id已经在数据库中有值了
+    """
+    sql = "select * from english_words where ID=?"
+    values = cur.execute(sql,(id,)).fetchall()
+    num = len(values)
+    if num > 0:
+        return True
+    else:
+        return False
+
+
+if __name__ == '__main__':
+    conn = sqlite3.connect(script_path+"/ggsddu.db")
+    cur = conn.cursor()
+    
+    handle_english_words()
+
+    cur.close()    
+    conn.close()
--- scripts/ggsddu.py
+++ scripts/ggsddu.py
@@ -1,83 +1,252 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
-
+import os
+import sys
+import sqlite3
+# import platform
 from tkinter import Tk
 from PIL import Image, ImageTk
 from tkinter import Label
+from tkinter import Canvas
+from tkinter import Entry
+from tkinter import Button
+from tkinter import StringVar
 
-# 初始化Tk()
-the_window = Tk()
+FONT_NAME = 'Century Schoolbook L'
 
-# 全局的初始按钮
-image_bkg = Image.open("D:\\code\\english_words\\main2.png")
-photo_bkg = ImageTk.PhotoImage(image_bkg)
+SCRIPT_PATH = os.path.split(os.path.realpath(sys.argv[0]))[0]
 
-image_yuwen = Image.open("D:\\code\\english_words\\yuwen.png")
-photo_yuwen = ImageTk.PhotoImage(image_yuwen)
-image_shuxue = Image.open("D:\\code\\english_words\\shuxue.png")
-photo_shuxue = ImageTk.PhotoImage(image_shuxue)
-image_yinyu = Image.open("D:\\code\\english_words\\yinyu.png")
-photo_yinyu = ImageTk.PhotoImage(image_yinyu)
+class App:
+    def __init__(self, frame):
+        self.frame = frame
+        
+        # 这里先定义所有科目widgets
+        self.subject_label_bkg = None
+        self.subject_label_yuwen = None
+        self.subject_label_shuxue = None
+        self.subject_label_yinyu = None
 
-# 全局科目按钮列表
-subject_list = []
+        # 定义单项填空题用到的widgets
+        self.single_blank_label_type = None     # 类型
+        self.single_blank_canvas_stem = None    # 题干画布
+        self.single_blank_text_stem = None      # 题干中的text
+        self.single_blank_label_input = None    
+        self.single_blank_entry_answer = None   # 输入框
+        self.single_blank_button_check = None   # 检查答案按钮
+        self.single_blank_button_next = None    # 下一步按钮
+        self.single_blank_button_return = None  # 返回按钮
+        self.single_blank_label_answer = None  
 
-def init_windows():
-    global the_window
-    #设置标题
-    the_window.title('The notebook of wrong questions for Li Zhenzhen')
-    the_window.state("zoomed")
-    the_window.resizable(width=False, height=False)
+        # 定义英语单词题用到的数据
+        self.data_single_blank_stem_string = ''                    # 题干
+        self.data_single_blank_is_correct = False                  # 答对没有
+        self.data_single_blank_is_lastone = False                  # 还有没有下一题
+        self.data_single_blank_answer_string = ''                  # 答案
+        self.data_single_blank_answer_variable = StringVar()       # 答案变量
+        self.data_single_blank_answer_times = 0                    # 做过几次
+        self.data_single_blank_answer_times_variable = StringVar() # 做过几次变量
+        self.data_single_blank_correct_times = 0                   # 答对过几次
+        self.data_single_blank_correct_variable = StringVar()      # 答对次数变量
+        self.data_single_blank_correct_rate = 0.0                  # 答对率
+        self.data_single_blank_correct_rate_variable = StringVar() # 答对率变量
 
+        # 题目数据
+        self.exercises_num = 0
+        self.current_num = -1     # -1 代表未开始
+        self.exercises_list = []
 
-def put_init_image():
-    global the_window
-    global photo_bkg, photo_yuwen, photo_shuxue, photo_yinyu
- 
-    image_label = Label(the_window,text='The notebook of wrong questions @ Li Zhenzhen',font=('MV BOLI',30),image=photo_bkg)
-    image_label.place(x=0, y=0, anchor='nw')
+        self.init_widgets()
+        self.entry()
 
-    label_yuwen = Label(the_window, image=photo_yuwen, cursor="spraycan")
-    subject_list.append(label_yuwen)
-    label_yuwen.bind('<Button-1>', enter_yuwen)
-    label_yuwen.place(x=150, y=300, anchor='nw')
+    def init_widgets(self):
+                # 进入科目入口
+        global photo_bkg, photo_yuwen, photo_shuxue, photo_yinyu
 
-    label_shuxue = Label(the_window, image=photo_shuxue, cursor="spraycan")
-    subject_list.append(label_shuxue)
-    label_shuxue.bind('<Button-1>', enter_shuxue)
-    label_shuxue.place(x=600, y=300, anchor='nw')
+        image_bkg = Image.open(SCRIPT_PATH+"/../resource/main.png")
+        photo_bkg = ImageTk.PhotoImage(image_bkg)
+        image_yuwen = Image.open(SCRIPT_PATH+"/../resource/yuwen.png")
+        photo_yuwen = ImageTk.PhotoImage(image_yuwen)
+        image_shuxue = Image.open(SCRIPT_PATH+"/../resource/shuxue.png")
+        photo_shuxue = ImageTk.PhotoImage(image_shuxue)
+        image_yinyu = Image.open(SCRIPT_PATH+"/../resource/yinyu.png")
+        photo_yinyu = ImageTk.PhotoImage(image_yinyu)
     
-    label_yinyu = Label(the_window, image=photo_yinyu, cursor="spraycan")
-    subject_list.append(label_yinyu)
-    label_yinyu.bind('<Button-1>', enter_yinyu)
-    label_yinyu.place(x=1050, y=300, anchor='nw')
+        self.subject_label_bkg = Label(self.frame,text='',image=photo_bkg)
 
-def enter_yuwen(event):
-    clear_frame()
+        self.subject_label_yuwen = Label(self.frame, image=photo_yuwen, cursor="spraycan")
+        self.subject_label_yuwen.bind('<Button-1>', self.enter_yuwen)
+
+        self.subject_label_shuxue = Label(self.frame, image=photo_shuxue, cursor="spraycan")
+        self.subject_label_shuxue.bind('<Button-1>', self.enter_shuxue)
+        
+        self.subject_label_yinyu = Label(self.frame, image=photo_yinyu, cursor="spraycan")
+        self.subject_label_yinyu.bind('<Button-1>', self.enter_yinyu)
+    
+        self.single_blank_label_type = Label(self.frame,text='Vocabulary:',font=(FONT_NAME,20,'bold'))
+        
+        self.single_blank_canvas_stem = Canvas(self.frame)
+        self.single_blank_canvas_stem.config(width=1000,height=400, relief='solid')
+
+        self.single_blank_label_input = Label(self.frame,text='Input words:',font=(FONT_NAME,20,'bold'))
+
+        self.single_blank_entry_answer = Entry(self.frame, font=(FONT_NAME,16,'bold'), width=65)
+
+        self.single_blank_button_check = Button(self.frame, text="Check", command=self.check_answer,
+            font=(FONT_NAME,20,'bold'), width=11, height=1)
+
+        self.single_blank_label_answer = Label(self.frame, textvariable=
+            self.data_single_blank_answer_variable,font=(FONT_NAME,20,'bold'))
+                
+        self.single_blank_button_next = Button(self.frame, text="Next", command=self.next,
+            font=(FONT_NAME,20,'bold'), width=11, height=1)
+
+        self.single_blank_button_return = Button(self.frame, text="Return", command=self.entry,
+            font=(FONT_NAME,20,'bold'), width=11, height=1)
 
 
-def enter_shuxue(event):
-    clear_frame()
+    def entry(self):
+        self.show_hide_single_blank_widgets(False)
+        self.show_hide_subject_widgets(True)
 
 
-def enter_yinyu(event):
-    clear_frame()
+    def enter_yuwen(self, event):
+        pass
 
 
-def clear_frame():
-    global subject_list
-    for i in range(len(subject_list)):
-        button = subject_list[i]
-        button.place_forget() 
+    def enter_shuxue(self, event):
+        pass
 
 
-def show_beidanci():
-    pass
+    def enter_yinyu(self, event):
+        self.show_hide_subject_widgets(False)
+        self.show_hide_single_blank_widgets(True)
+        sql = "select * from english_words"
+        self.exercises_list = cur.execute(sql).fetchall()
+        self.exercises_num = len(self.exercises_list)
+        self.current_num = -1  # 切回未开始, 掉next()会+1
+        self.next()
+
+
+    def check_answer(self):
+        user_answer = self.single_blank_entry_answer.get()
+        # print(user_answer)
+        self.data_single_blank_is_correct = \
+            user_answer.strip() == self.data_single_blank_answer_string
+
+        if self.data_single_blank_is_correct:
+            correct_string = "Correct"
+            self.single_blank_label_answer.configure(fg="green")
+        else:
+            correct_string = "Wrong"
+            self.single_blank_label_answer.configure(fg="red")
+        self.data_single_blank_answer_variable.set("%s. The words is: %s" %
+            (correct_string, self.data_single_blank_answer_string))
+        self.single_blank_label_answer.place(x=100, y=580, anchor='nw')
+        if self.data_single_blank_is_lastone:
+            # print("show return")
+            self.single_blank_button_return.place(x=1100, y=580, anchor='nw')
+        else:
+            # print("show next")
+            self.single_blank_button_next.place(x=1100, y=580, anchor='nw')
+
+
+    def next(self):
+        self.single_blank_button_next.place_forget()
+        self.single_blank_button_return.place_forget()
+        self.single_blank_label_answer.place_forget()
+        self.single_blank_entry_answer.delete(0, 'end')
+
+        self.current_num += 1
+        self.data_single_blank_is_lastone = \
+            self.current_num == self.exercises_num-1
+        # print(self.current_num, self.exercises_num, self.data_single_blank_is_lastone)
+        exercise = self.exercises_list[self.current_num]
+        
+        self.data_single_blank_stem_string = exercise[1]
+        # 如果有值则删除它
+        if self.single_blank_text_stem:
+            self.single_blank_canvas_stem.delete(self.single_blank_text_stem)
+        self.single_blank_text_stem = \
+            self.single_blank_canvas_stem.create_text((10, 10), 
+            text=self.data_single_blank_stem_string,
+            font=(FONT_NAME,16), anchor='nw', width=1000)
+        self.data_single_blank_answer_string = exercise[2]
+
+
+    def show_hide_subject_widgets(self, is_show):
+        # 显示/隐藏 科目组件
+        if is_show:
+            self.subject_label_bkg.place(x=0, y=0, anchor='nw')
+            self.subject_label_yuwen.place(x=150, y=300, anchor='nw')
+            self.subject_label_shuxue.place(x=600, y=300, anchor='nw')
+            self.subject_label_yinyu.place(x=1050, y=300, anchor='nw')
+        else:
+            self.subject_label_bkg.place_forget()
+            self.subject_label_yuwen.place_forget()
+            self.subject_label_shuxue.place_forget()
+            self.subject_label_yinyu.place_forget()
+
+
+    def show_hide_single_blank_widgets(self, is_show):
+        # 显示/隐藏 单项填空组件
+        if is_show:
+            self.single_blank_label_type.place(x=100, y=50, anchor='nw')
+            # 如果有值则删除它
+            if self.single_blank_text_stem:
+                self.single_blank_canvas_stem.delete(self.single_blank_text_stem)
+            self.single_blank_text_stem = \
+                self.single_blank_canvas_stem.create_text((10, 10), 
+                    text=self.data_single_blank_stem_string,
+                    font=(FONT_NAME,16), anchor='nw', width=1000)
+            self.single_blank_canvas_stem.place(x=100,y=100)
+            self.single_blank_label_input.place(x=100, y=520, anchor='nw')
+            self.single_blank_entry_answer.place(x=300, y=522, anchor='nw')
+            self.single_blank_entry_answer.focus ()
+            self.single_blank_button_check.place(x=1100, y=515, anchor='nw')
+            # 点击check_button才能显示
+            # self.single_blank_label_answer.place(x=100, y=580, anchor='nw')
+            # self.single_blank_button_next.place(x=1100, y=580, anchor='nw')
+        else:
+            self.single_blank_label_type.place_forget()
+            self.single_blank_canvas_stem.place_forget()
+            self.single_blank_label_input.place_forget()
+            self.single_blank_entry_answer.place_forget()
+            self.single_blank_button_check.place_forget()
+            self.single_blank_label_answer.place_forget()
+            self.single_blank_button_next.place_forget()
+            self.single_blank_button_return.place_forget()
 
 
 if __name__ == '__main__':
-    put_init_image()
-    init_windows()
+    # 创建主窗口
+    root = Tk()
+    root.title('The notebook of wrong questions for Li Zhenzhen')
+    '''
+    if(platform.system()=='Windows'):
+        the_window.state("normal")
+        the_window.resizable(width=False, height=False)
+    elif(platform.system()=='Linux'):
+        w = the_window.winfo_screenwidth()
+        h = the_window.winfo_screenheight()
+        the_window.geometry("%dx%d" %(w, h))
+    else:
+        print('Can only support windows and linux')
+    '''
+    width = 1600
+    heigh = 768
+    screenwidth = root.winfo_screenwidth()
+    screenheight = root.winfo_screenheight()
+    root.geometry('%dx%d+%d+%d'%(width, heigh, (screenwidth-width)/2, (screenheight-heigh)/2))
+    root.resizable(0,0) #防止用户调整尺寸
+
+    # 建立App
+    App(root)
+
+    conn = sqlite3.connect(SCRIPT_PATH+"/../database/ggsddu.db")
+    cur = conn.cursor()
 
     #进入消息循环
-    the_window.mainloop()
+    root.mainloop()
+
+    cur.close()    
+    conn.close()
